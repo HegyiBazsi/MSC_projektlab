e4444158bd3052e46327f8e3f223f10582dd1128
csibe93 <35483722+csibe93@users.noreply.github.com>
2019-10-13 11:59:46 +0200

    v1.5
    Model updated, test added

--- AI/model.py
+++ AI/model.py
@@ -1,20 +1,18 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Flatten
+from warnings import filterwarnings
 import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import pickle
 from keras.layers import Convolution2D, MaxPooling2D
-
-from sklearn.utils import shuffle
+from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
+from keras import backend as k
+k.set_image_dim_ordering('th')
 
-from keras import backend as K
-K.set_image_dim_ordering('th')
-from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers.core import Dense, Flatten
-from sklearn import preprocessing
-from warnings import filterwarnings
 
 # W A R N I N G    H A N D L I N G
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -28,12 +26,12 @@ data_dir_list = os.listdir(data_path)
 img_rows = 128
 img_cols = 128
 num_channel = 1
-num_epoch = 10
 
 # Number of classes
 num_of_classes = 4
 
 img_data_list = []
+labels = []
 
 for dataset in data_dir_list:
     img_list = os.listdir(data_path + '/' + dataset)
@@ -41,95 +39,40 @@ for dataset in data_dir_list:
     for img in img_list:
         input_img = cv2.imread(data_path + '/' + dataset + '/' + img)
         input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-        input_img_resize = cv2.resize(input_img,(128, 128))
+        input_img_resize = cv2.resize(input_img, (128, 128))
         img_data_list.append(input_img_resize)
+
+        label = dataset.split(os.path.sep)[-1]
+        labels.append(label)
 
 img_data = np.array(img_data_list)
 img_data = img_data.astype('float32')
 img_data /= 255
+labels = np.array(labels)
 print(img_data.shape)
 
 if num_channel == 1:
-    if K.image_dim_ordering() == 'th':
+    if k.image_dim_ordering() == 'th':
         img_data = np.expand_dims(img_data, axis=1)
         print(img_data.shape)
     else:
         img_data = np.expand_dims(img_data, axis=4)
         print(img_data.shape)
 else:
-    if K.image_dim_ordering() == 'th':
+    if k.image_dim_ordering() == 'th':
         img_data = np.rollaxis(img_data, 3, 1)
         print(img_data.shape)
 
-USE_SKLEARN_PREPOCESSING = False
 
-if USE_SKLEARN_PREPOCESSING:
-    def image_to_feature_vector(image, size=(128, 128)):
-        return cv2.resize(image, size).flatten()
-
-    img_data_list = []
-    for dataset in data_dir_list:
-        img_list = os.listdir(data_path + '/' + dataset)
-        print("Loaded the images of dataset-"+"{}\n".format(dataset))
-        for img in img_list:
-            input_img = cv2.imread(data_path + "/" + dataset + "/" + img)
-            input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-            input_img_flatten = image_to_feature_vector(input_img, (128, 128))
-            img_data_list.append(input_img_flatten)
-
-    img_data = np.array(img_data_list)
-    img_data = img_data.astype('float32')
-    print(img_data.shape)
-    img_data_scaled = preprocessing.scale(img_data)
-    print(img_data_scaled.shape)
-
-    print(np.mean(img_data_scaled))
-    print(np.std(img_data_scaled))
-
-    print(img_data_scaled.mean(axis=0))
-    print(img_data_scaled.std(axis=0))
-
-    if K.image_dim_ordering() == 'th':
-        img_data_scaled = img_data_scaled.reshape(img_data.shape[0], num_channel, img_rows, img_cols)
-        print(img_data_scaled.shape)
-
-    else:
-        img_data_scaled = img_data_scaled.reshape(img_data.shape[0], img_rows, img_cols, num_channel)
-        print(img_data_scaled.shape)
-
-    if K.image_dim_ordering() == 'th':
-        img_data_scaled = img_data_scaled.reshape(img_data.shape[0], num_channel, img_rows, img_cols)
-        print(img_data_scaled.shape)
-
-    else:
-        img_data_scaled = img_data_scaled.reshape(img_data.shape[0], img_rows, img_cols, num_channel)
-        print(img_data_scaled.shape)
-
-if USE_SKLEARN_PREPOCESSING:
-    img_data = img_data_scaled
-
-# LABELING
-num_classes = 4
-num_of_samples = img_data.shape[0]
-labels = np.ones((num_of_samples,),dtype='int64')
-
-labels[0:202] = 0
-labels[202:404] = 1
-labels[404:606] = 2
-labels[606:] = 3
-
-names = ["rect", "star", "circle", "else"]
-
-# One hot coding
-Y = np_utils.to_categorical(labels, num_classes)
-
-# Shaffle
-x, y = shuffle(img_data, Y, random_state=2)
+# binarize the labels
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
 
 # Split
-X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+X_train, X_test, Y_train, Y_test = train_test_split(img_data, labels, test_size=0.2, random_state=2)
 
 # Model variables
+num_epoch = 2
 input_shape = img_data[0].shape
 number_of_filters = 32                      # Integer, the dimensionality of the output space
 kernel_size = (3, 3)                        # An integer or tuple/list of 2 integers, specifying
@@ -172,29 +115,41 @@ model.layers[0].get_weights()
 np.shape(model.layers[0].get_weights()[0])
 
 # Training
-model_log = model.fit(  X_train, Y_train,
-                        batch_size=None,
-                        epochs=num_epoch,
-                        steps_per_epoch=100,
-                        verbose=1,
-                        validation_data=(X_test, Y_test),
-                        validation_steps=10)
+model_log = model.fit(X_train, Y_train,
+                      batch_size=None,
+                      epochs=num_epoch,
+                      steps_per_epoch=100,
+                      verbose=1,
+                      validation_data=(X_test, Y_test),
+                      validation_steps=10)
 
 loss, accuracy = model.evaluate(X_test, Y_test, verbose=False)
 
-plt.plot(model_log.history['acc'])
-plt.plot(model_log.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['training', 'validation'], loc='best')
+# P L O T   T H E    T R A I N G    L O S S     A N D   A C C U R A C Y
+plt.style.use('dark_background')
+plt.figure()
+plt.plot(np.arange(0, num_epoch), model_log.history["loss"], label="train_loss")
+plt.plot(np.arange(0, num_epoch), model_log.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, num_epoch), model_log.history["acc"], label="train_acc")
+plt.plot(np.arange(0, num_epoch), model_log.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy on Data set")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
 plt.show()
 
-# # Save
-# model_json = model.to_json()
-# with open("model.json", "w") as json_file:
-#     json_file.write(model_json)
-# # serialize weights to HDF5
-# model.save_weights("model.h5")
-model.save("model.h5")
+# Save
+saveFormatElse = "C:\\Users\\rrb12\\Desktop\\projLab\\model\\" \
+                 "\\model{:>01}.h"
+path, dirs, files = next(os.walk("C:\\Users\\rrb12\\Desktop\\projLab\\model\\"))
+file_count = len(files)
+
+model.save(saveFormatElse.format(file_count))  # Saving the model
+model.summary()                     # Summarizing the train
 print("Saved model to disk")
+
+# Save the label binarizer to disk
+print("Serializing label binarizer...")
+f = open("lb.pickle", "wb")
+f.write(pickle.dumps(lb))
+f.close()
